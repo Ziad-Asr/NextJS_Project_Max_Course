11bacfb37ba6f15afdaa75fd637e0716c15e0a6d
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-06-27 17:31:37 +0300

    update

--- app/page.js
+++ app/page.js
@@ -1,27 +1,9 @@
-import Link from "next/link";
-import Header from "@/components/header";
-
 export default function Home() {
-  console.log("Next.js is here"); //This will appear in the terminal. (Not in the browser console.)
-
   return (
     <main>
-      <Header />
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p>
-        <Link href="/about">About US</Link>
-      </p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
-
-// We name this file (Page). that ia {{a must}} because the file that named page will be the root file of this project.
-// (the name of the compaonent inside it {like here name (home) does not matter if it is different})
-
-// *******
-// *** Important *** ANY THING RELATED TO NAMING, IT IS ONLY APPLIES INSIED THE ((APP)) FOLDER, OUTSIDE IT THEY BECOME ONLY FOR MY PREFER. *****
-// *******
-
-// This is called a (Server component) in Next.js
-// When I type (Console.log('...')), this will appear in the terminal her not in the browser console => that is beacuse it's a server component
-// The (about) folder will hold the routes of our app.
