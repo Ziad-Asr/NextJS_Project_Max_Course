513fae59a9ba2d99a02be9516b585e93692156af
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-06-27 22:38:08 +0300

    update

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,26 @@
-const MealsPage = () => {
-  return (
-    <main>
-      <h1>MealsPage</h1>
-    </main>
-  );
-};
+import Link from "next/link";
 
-export default MealsPage;
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
+}
