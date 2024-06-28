dfd0c875f60f1b6390b258d11704fb44bd87410c
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-06-28 11:06:48 +0300

    update

--- app/meals/page.js
+++ app/meals/page.js
@@ -8,7 +8,7 @@ export default function MealsPage() {
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created{" "}
+          Delicious meals, created
           <span className={classes.highlight}>by you</span>
         </h1>
         <p>
